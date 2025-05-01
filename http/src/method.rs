bbabd548b2bba99f4e31f685ae288e677b319aa7
GSGFs <gsgfs@gsgfs.moe>
2025-05-01 23:14:29 +0800

    feat(HttpMethod): add support check for HTTP methods

--- http/src/method.rs
+++ http/src/method.rs
@@ -15,6 +15,20 @@ impl From<&str> for HttpMethod {
     }
 }
 
+impl From<String> for HttpMethod {
+    fn from(value: String) -> Self {
+        HttpMethod::from(value.as_str())
+    }
+}
+
+impl HttpMethod {
+    /// 'method' can be anything which can into `HttpMethod`
+    pub fn is_support<T: Into<HttpMethod>>(method: T) -> bool {
+        let m: HttpMethod = method.into();
+        m != HttpMethod::NoSupport
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -22,6 +36,19 @@ mod tests {
     #[test]
     fn test_method_into() {
         assert_eq!(HttpMethod::from("GET"), HttpMethod::Get);
+        assert_eq!(HttpMethod::from("POST"), HttpMethod::Post);
         assert_eq!(HttpMethod::from("some str"), HttpMethod::NoSupport);
+
+        assert_eq!(HttpMethod::from("GET".to_string()), HttpMethod::Get);
+    }
+
+    #[test]
+    fn test_method_is_support() {
+        assert!(HttpMethod::is_support("GET"));
+        assert!(HttpMethod::is_support("POST"));
+        assert!(!HttpMethod::is_support("some str"));
+        assert!(HttpMethod::is_support(HttpMethod::Get));
+        assert!(HttpMethod::is_support(HttpMethod::Post));
+        assert!(!HttpMethod::is_support(HttpMethod::NoSupport));
     }
 }
