e4168d04a615e3b473048aad9b722c195584987a
GSGFs <gsgfs@gsgfs.moe>
2025-05-02 17:17:54 +0800

    test: add test page

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,21 @@ use tokio::fs;
 #[tokio::main]
 async fn main() {
     let home = fs::read("./src/www/html/home.html").await.unwrap();
+    let test = fs::read("./src/www/html/test.html").await.unwrap();
 
     let mut binding = HttpRouter::new();
     let router = binding
-        .add(HttpMethod::Get, "/", Arc::new(move |_| {
-            HttpResponse::new(200, "OK").with_body(HttpBody::from(&home))
-        }))
+        .add(
+            HttpMethod::Get,
+            "/",
+            Arc::new(move |_| HttpResponse::new(200, "OK").with_body(HttpBody::from(&home))),
+        )
+        .await
+        .add(
+            HttpMethod::Get,
+            "/test",
+            Arc::new(move |_| HttpResponse::new(200, "OK").with_body(HttpBody::from(&test))),
+        )
         .await;
 
     let mut server = HttpServer::new();
